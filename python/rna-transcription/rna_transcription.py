1583bd641e3c4bdefbe0749a317506edabc2967e
cdrainxv <cdrainxv@users.noreply.github.com>
2018-04-29 21:48:58 -0600

    add rna_transcription solution

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,13 @@
 def to_rna(dna_strand):
-    pass
+    rna_dict = {
+        'G': 'C',
+        'C': 'G',
+        'T': 'A',
+        'A': 'U'
+    }
+
+    rna_strand = ''
+    for char in dna_strand:
+        rna_strand += rna_dict[char]
+
+    return rna_strand
