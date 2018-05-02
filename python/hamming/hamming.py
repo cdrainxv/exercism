b19f7df650cad0ed401f909064b48988a17b7a56
cdrainxv <cdrainxv@users.noreply.github.com>
2018-05-02 14:21:24 -0600

    update hamming solution

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -2,10 +2,4 @@ def distance(strand_a, strand_b):
     if len(strand_a) != len(strand_b):
         raise ValueError('Length of strands are not equal')
 
-    count = 0
-    
-    for a, b in zip(strand_a, strand_b):
-        if a != b:
-            count += 1
-
-    return count
+    return sum(a != b for a, b in zip(strand_a, strand_b))
